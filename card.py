fef2e50257c7ffe592498e289af2700245f00317
12312 <t@e.com>
2018-09-28 10:45:22 +0800

    添加识别坐标以后的去重功能

--- card.py
+++ card.py
@@ -24,7 +24,16 @@ class Card:
 
 def get_crd(sh, tmp, thd):  # get the coordinates of cards/marks
     ary = location(sh, tmp, thd)
-    return [(i, list(ary.keys())[0]) for i in list(ary.values())[0]]
+    ary=[(i, list(ary.keys())[0]) for i in list(ary.values())[0]]
+    for i in range(len(ary)):
+        for p in range(1, len(ary) - i):
+            for k in range(-5, 5):
+                for l in range(-5, 5):
+                    if ((ary[i][0] + k) == ary[i + p][0]) and ((ary[i][1] + l) == ary[i + p][1]):
+                        ary[i + p] = [0, 0]
+    while ary.count([0, 0]) >= 1:
+        ary.remove([0, 0])
+    return ary
 
 
 # This part is used to set the status of the cards
